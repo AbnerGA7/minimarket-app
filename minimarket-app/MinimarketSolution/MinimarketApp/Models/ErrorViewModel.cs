f060caefd861634f39d76e0ef1daca0370c4b05a
Abner Gonzales <abner.gonzales.work@gmail.com>
2025-11-21 21:38:42 -0500

    Add files via upload

---
+++ minimarket-app/MinimarketSolution/MinimarketApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinimarketApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
